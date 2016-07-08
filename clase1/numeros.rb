bfae4d17778607361ef614468a48de3af0e1fdaa
Daniel Iturriza <diturriza17@gmail.com>
2016-07-08 18:20:55 -0400

    re-order

---
+++ clase1/numeros.rb
@@ -0,0 +1,23 @@
+puts 92*12 #multiplicacion
+
+puts 1+2 #suma
+
+puts 2-1 #resta
+
+puts 5/2 #division
+
+#Los numeros son objetos, metodos como next, pred
+#están establecidos para los números, zero
+
+# irb(main):001:0> 8.next
+# => 9
+# irb(main):002:0> 9.pred
+# => 8
+# irb(main):003:0> 8.zero?
+# => false
+# irb(main):004:0> 8.odd?
+# => false
+# irb(main):005:0> 8.even?
+# => true
+# irb(main):006:0> 8.class
+# => Fixnum
