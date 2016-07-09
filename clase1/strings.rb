22b1ea2afd284118b235d6202b255361ae0a7e78
Daniel Iturriza <diturriza17@gmail.com>
2016-07-08 23:02:13 -0400

    strings

---
+++ clase1/strings.rb
@@ -0,0 +1,16 @@
+puts "ruby es divertido".capitalize
+
+puts "DANIEL@GMAIL.COM".downcase
+
+puts "RUBY es DIVERTIDO".swapcase
+
+puts "alumnos de platiz".insert(0, "Hola ")
+
+puts "daniel iturriza".reverse
+
+puts "ruby es divretido divretido".gsub('divretido','divertido')
+puts "ruby es divretido divretido".sub('divretido','divertido')
+puts "alumnos de platzi".gsub('a','@')
+puts "  alumnso de platzi  ".strip
+
+puts "a".next
