573107fba59628c8c265ce25f7ca57572c1f1685
Daniel Iturriza <diturriza17@gmail.com>
2016-07-08 22:44:10 -0400

    logics operators

---
+++ clase1/operadores.rb
@@ -0,0 +1,8 @@
+edad = 23
+mayor_de_edad = 18
+
+puts (edad == mayor_de_edad)
+
+puts (edad >= mayor_de_edad)
+
+puts "mayor de edad" == "mayor de EDAD"
---
+++ clase1/operadores_programa.rb
@@ -0,0 +1,11 @@
+edad = 30
+mayor_de_edad = 18
+
+if edad < mayor_de_edad
+  puts "eres menor de edad, no puedes entrar"
+
+else
+  puts "Entonces puedes tomar unas curdas"
+end
+
+puts "final"
